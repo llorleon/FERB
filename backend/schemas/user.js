22187b50fb3dd6e8e9e3e20190aefde520fa1ee4
llorleon <victor.martin.lloren@gmail.com>
2023-04-23 11:40:53 +0200

    Implementacion BackEnd y Front

---
+++ backend/schemas/user.js
@@ -0,0 +1,18 @@
+export default {
+
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'user',
+            title: 'User',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        },
+    ]
+}
